cbd73d174e6131cd22c51b8c06a1cfe1cf9291c1
Bolot Ashirkulov <ashirkulovbolott@gmail.com>
2023-01-03 23:12:55 +0600

    added file .idea

---
+++ src/kg/attractor/java/Main.java
@@ -0,0 +1,26 @@
+package kg.attractor.java;
+
+// import static java.util.stream.Collectors.*;
+// import static java.util.Comparator.*;
+
+// используя статические imports
+// мы импортируем *всё* из Collectors и Comparator.
+// теперь нам доступны такие операции как
+// toList(), toSet() и прочие, без указания уточняющего слова Collectors. или Comparator.
+// вот так было до импорта Collectors.toList(), теперь стало просто toList()
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // это для домашки
+        // выберите любое количество заказов, какое вам нравится.
+
+        //var orders = RestaurantOrders.read("orders_100.json").getOrders();
+        //var orders = RestaurantOrders.read("orders_1000.json").getOrders();
+        //var orders = RestaurantOrders.read("orders_10_000.json").getOrders();
+
+        // протестировать ваши методы вы можете как раз в этом файле (или в любом другом, в котором вам будет удобно)
+    }
+}
